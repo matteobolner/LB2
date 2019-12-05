e9837ed5af083f51b4fc7945805af5475fc8aa05
PelmoBolner <matteo.bolner@studio.unibo.it>
2019-12-05 12:48:00 +0100

    updates

--- scripts/get_aa_ss_from_extracted.py
+++ scripts/get_aa_ss_from_extracted.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 
 def get_fasta_from_extracted(dssp_extracted_folder, id_file):
+    
     open_id_file = open(id_file, "r")
     id_chain_dict = {}
     for line in open_id_file:
@@ -13,44 +14,46 @@ def get_fasta_from_extracted(dssp_extracted_folder, id_file):
     complete_bs = open("complete_bs.fasta", "a")
     
     for file in os.listdir(dssp_extracted_folder):
-        df = str(dssp_extracted_folder) + "/" + file
-        dssp_file_path = Path(df)
+        
+        dssp_filename = str(dssp_extracted_folder) + "/" + file
+        dssp_file_path = Path(dssp_filename)
         file_id_only = file.split(".")[0]
-        #print(file_id_only)
-        ff = str(fasta_files_folder) + "/" + file_id_only + "_" + id_chain_dict[file_id_only]
-        fasta_file_path = Path(ff)
         a = dssp_file_path.open()  
-        id_and_fasta_file = open(fasta_file_path, "r")
-        for line in id_and_fasta_file:
-            cleanline = line.rstrip()
-            #print(cleanline)
-            print(len(cleanline))
-            complete_bs.write(line)
 
+        aa_seq_string = ""
         ss_seq_string = ""
 
         for line in a:
             splitted_line = line.split(":")
-            
             chain = splitted_line[0]
-            #print(chain)
-            #print(id_chain_dict[file_id_only])
-            if chain == id_chain_dict[file_id_only]:
-                ss_seq = splitted_line[2]
-                ss_seq_string += ss_seq
-        #print(ss_seq_string)
-        print(len(ss_seq_string))
+            residue = splitted_line[1]
+            secondary_structure = splitted_line[2]
 
-        complete_bs.write(ss_seq_string + "\n")
+            correct_ss = "PROBLEM"
+
+            if secondary_structure in ("H", "G", "I"):
+                correct_ss = "H"
+                print(correct_ss)
+            elif secondary_structure in ("B", "E"):
+                correct_ss = "E"
+                print(correct_ss)
+            elif secondary_structure in ("T", "S", " "):
+                correct_ss = "C"
+                print(correct_ss)
+
+            if chain == id_chain_dict[file_id_only]:
+                aa_seq_string += residue
+                ss_seq_string += correct_ss
+        
+        id_and_chain = file_id_only + "_" + id_chain_dict[file]
+
+        complete_bs.write(">" + id_and_chain + "\n" + aa_seq_string.upper() + "\n" + ss_seq_string + "\n" + "\n")
         
     return()
 
-''' situation at the moment: the program works as intended but there is the problem of pdb sequences being different from the fasta obtained from pdb; the best solution apparently is to just delete all sequences with different sequences, which would mean adding other new sequences fromt the "whole" blind set
-OR, another solution could be to just use the dssp for both sequence and ss, putting the aa sequence in all caps to avoid the problem with the bisulpide bonds '''
 
 
 if __name__ == "__main__":
     dssp_extractedfolder = sys.argv[1]
     id_file = sys.argv[2]
-    fasta_files_folder = sys.argv[3]
-    get_fasta_from_extracted(dssp_extractedfolder, id_file, fasta_files_folder)
+    get_fasta_from_extracted(dssp_extractedfolder, id_file)
