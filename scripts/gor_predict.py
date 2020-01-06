4971fc8e8f6d6b6b42c40e7860cbc5dc2a470110
PelmoBolner <matteo.bolner@studio.unibo.it>
2020-01-06 15:58:20 +0100

    script updates

--- scripts/gor_predict.py
+++ scripts/gor_predict.py
@@ -3,95 +3,103 @@ import os
 import numpy as np
 np.set_printoptions(threshold=np.inf)
 
-#input: 1)a folder containing the sequence profiles of the input proteins 
-#       2)the folder containing the GOR model
 
+#window size - must be the same as the training
 w = 17
 
+
+
+#functions to load the training model matrices
 def get_h(model_folder):
     H_path = os.path.join(model_folder + "H_MATRIX.txt")
-    H_matrix = np.loadtxt(H_path)
+    H_matrix = np.loadtxt(H_path, dtype= 'float64')
     return(H_matrix)
 
 def get_e(model_folder):
     E_path = os.path.join(model_folder + "E_MATRIX.txt")
-    E_matrix = np.loadtxt(E_path)
+    E_matrix = np.loadtxt(E_path, dtype= 'float64')
     return(E_matrix)
 
 def get_c(model_folder):
     C_path = os.path.join(model_folder + "C_MATRIX.txt")
-    C_matrix = np.loadtxt(C_path)
+    C_matrix = np.loadtxt(C_path, dtype= 'float64')
     return(C_matrix)
 
-def get_counter(model_folder):
-    counting_path = os.path.join(model_folder + "COUNTING_MATRIX.txt")
-    counting_matrix = np.loadtxt(counting_path, usecols=2)
-    return(counting_matrix)
+def get_ss_counter(model_folder):
+    ss_count_path = os.path.join(model_folder + "ss_count_matrix.txt")
+    ss_count_matrix = np.loadtxt(ss_count_path, dtype= 'float64', usecols=2)
+    return(ss_count_matrix)
 
 def get_aa_freqs(model_folder):
-    total_path = os.path.join(model_folder + "TOT_MATRIX.txt")
-    total_matrix = np.loadtxt(total_path)
-    return(total_matrix)
+    aa_freq_path = os.path.join(model_folder + "aa_freq_matrix.txt")
+    aa_freq_matrix = np.loadtxt(aa_freq_path, dtype= 'float64')
+    return(aa_freq_matrix)
+
+
 
 def predict_from_set(model_folder):
+    #function to iterate over the list of test ids and predict the ss sequence from them
+    open_idlist = open(id_list, "r")
+
     H_matrix = get_h(model_folder)
-    #print(H_matrix)
     E_matrix = get_e(model_folder)
     C_matrix = get_c(model_folder)
-    counting_matrix = get_counter(model_folder)
-    total_matrix = get_aa_freqs(model_folder)
-    open_idlist = open(id_list, "r")
+    ss_count_matrix = get_ss_counter(model_folder)
+    aa_freq_matrix = get_aa_freqs(model_folder)
+    
+
+    inf_matrix_H = np.log(np.divide(H_matrix, np.multiply(ss_count_matrix[0], aa_freq_matrix)))
+    inf_matrix_E = np.log(np.divide(E_matrix, np.multiply(ss_count_matrix[1], aa_freq_matrix)))
+    inf_matrix_C = np.log(np.divide(C_matrix, np.multiply(ss_count_matrix[2], aa_freq_matrix)))
+
+    print(np.divide(H_matrix, np.multiply(ss_count_matrix[0], aa_freq_matrix)))
+    print(np.divide(E_matrix, np.multiply(ss_count_matrix[1], aa_freq_matrix)))
+    print(np.divide(C_matrix, np.multiply(ss_count_matrix[2], aa_freq_matrix)))
     
     for line in open_idlist:
         profile_name = line.rstrip() + ".profile"
         profile_path = os.path.join(profile_folder, profile_name)
-        predict_ss(profile_path, H_matrix, E_matrix, C_matrix, counting_matrix, total_matrix)
+        predict_ss(profile_path, inf_matrix_H, inf_matrix_E, inf_matrix_C, ss_count_matrix, aa_freq_matrix)
    
-    return()
+    return(inf_matrix_H, inf_matrix_E, inf_matrix_C)
 
 
 
-def predict_ss(profile_file, H_matrix, E_matrix, C_matrix, counting_matrix, total_matrix):
+def predict_ss(profile_file, inf_matrix_H, inf_matrix_E, inf_matrix_C, ss_count_matrix, aa_freq_matrix):
+  
+    #open the profile and set up the padding for the window matrix
+  
     profile_opened = open(profile_file, "r")
     pad = (int(w)//2)
-    padding_matrix = np.zeros((pad, 20), dtype = 'i')
-    profile_matrix = np.loadtxt(profile_file, dtype= 'i')
-    #print(profile_matrix)
+    padding_matrix = np.zeros((pad, 20), dtype = 'float64')
+    profile_matrix = np.loadtxt(profile_file, dtype= 'float64')
     padded_profile = np.concatenate((padding_matrix,profile_matrix,padding_matrix), axis = 0 )
 
+    #initiate the empty ss sequence
     predicted_sequence = ""
+    
     c = -1
     for line in profile_opened:
         c += 1        
         current_profile_matrix = padded_profile[c:(c+17)]
-        #print(current_profile_matrix)
-        #print(np.multiply(counting_matrix[0], total_matrix))
-        print(np.sum(np.log(np.divide(E_matrix,np.multiply(counting_matrix[1], total_matrix)))))
-        #print(np.multiply(counting_matrix[1], total_matrix))
-
-        #I = log(SS_matrix/(SS_freq*AA_freq))
-        inf_matrix_H = np.log(np.divide(H_matrix, np.multiply(counting_matrix[0], total_matrix)))
-        inf_matrix_E = np.log(np.divide(E_matrix, np.multiply(counting_matrix[1], total_matrix)))
-        inf_matrix_C = np.log(np.divide(C_matrix, np.multiply(counting_matrix[2], total_matrix)))
-
-        #print(inf_matrix_H)
-        #print(inf_matrix_C)
-        #print(inf_matrix_E)
-
+        '''p_RS = H_matrix
+        p_R = aa_freq_matrix
+        p_S = ss_count_matrix[0]
+        p_SR_H = np.log(p_RS/(p_R*p_S))
+        print(p_SR_H)'''
+        
         infcont_H = np.sum(np.multiply(inf_matrix_H, current_profile_matrix))
-        #print(infcont_H)
         infcont_E = np.sum(np.multiply(inf_matrix_E, current_profile_matrix))
-        #print(infcont_E)
         infcont_C = np.sum(np.multiply(inf_matrix_C, current_profile_matrix))
-        #print(infcont_C)
-        #print(infcont_H)
+       
         infcont_dict = {infcont_H:"H",infcont_E:"E",infcont_C:"-"} 
         predicted_sequence += infcont_dict.get(max(infcont_dict)) 
-        #print(infcont_dict)  
-        break
-    print(predicted_sequence)
 
-    return(predicted_sequence)    
+    #print(predicted_sequence)
+
+    return(predicted_sequence)  
+
+
 if __name__ == "__main__":
     model_folder = sys.argv[1]
     id_list = sys.argv[2]
--- scripts/gor_train.py
+++ scripts/gor_train.py
@@ -3,20 +3,27 @@ import os
 import numpy as np
 np.set_printoptions(threshold=np.inf)
 
-w = 17
 
-E_matrix = np.zeros((int(w),20), dtype= 'i')
-H_matrix = np.zeros((int(w),20), dtype= 'i')
-C_matrix = np.zeros((int(w),20), dtype= 'i')
-TOT_matrix = np.zeros((int(w),20), dtype= 'i')
-counting_matrix = np.zeros((4,3), dtype= 'object')
 
-counting_matrix[0][0] = "H"
-counting_matrix[1][0] = "E"
-counting_matrix[2][0] = "-"
-counting_matrix[3][0] = "TOT"
+w = 17      #profile window size
 
-def fill_matrices(dssp_file,profile_file, H_matrix, E_matrix, C_matrix, TOT_matrix, counting_matrix):
+#initiate empty matrices
+H_matrix = np.zeros((int(w),20), dtype= 'float64')            #matrices containing the frequency of the residues corresponding to the considered ss
+E_matrix = np.zeros((int(w),20), dtype= 'float64')            
+C_matrix = np.zeros((int(w),20), dtype= 'float64')
+aa_freq_matrix = np.zeros((int(w),20), dtype= 'float64')      #matrix containing the total frequency of all residues
+ss_count_matrix = np.zeros((4,3), dtype= 'object')      #matrix containing the total number of ss with their corresponding frequency (independently of the residues)
+
+ss_count_matrix[0][0] = "H"                             #ss_count_matrix headers
+ss_count_matrix[1][0] = "E"
+ss_count_matrix[2][0] = "-"
+ss_count_matrix[3][0] = "TOT"
+
+
+
+def fill_matrices(dssp_file,profile_file, H_matrix, E_matrix, C_matrix, aa_freq_matrix, ss_count_matrix):
+    
+    #open the current dssp file and obtain the ss sequence
     dssp_opened = open(dssp_file, "r")
     for line in dssp_opened:
         if line[0] == ">":
@@ -24,75 +31,79 @@ def fill_matrices(dssp_file,profile_file, H_matrix, E_matrix, C_matrix, TOT_matr
         else:
             dssp_seq = line.rstrip()
 
+    #load the current sequence profile and initiate the window matrix with padding before and after the profile
     pad = (int(w)//2)
-    padding_matrix = np.zeros((pad, 20), dtype = 'i')
-    profile_matrix = np.loadtxt(profile_file, dtype= 'i')
+    padding_matrix = np.zeros((pad, 20), dtype = 'float64')
+    profile_matrix = np.loadtxt(profile_file, dtype= 'float64')
     padded_profile = np.concatenate((padding_matrix,profile_matrix,padding_matrix), axis = 0 )
-    #print(padded_profile)
+    
+
+    #iterate over the dssp sequence and add the current window matrix to the corresponding matrices 
     c = -1
     for i in dssp_seq:
         c += 1
-        window_matrix = padded_profile[c:(c+17)]
-        #print(window_matrix)
+        window_matrix = np.divide(padded_profile[c:(c+17)],100)
+        
         if i == "H":
-            H_matrix += window_matrix
-            TOT_matrix += window_matrix
-            counting_matrix[0][1] += 1
-            counting_matrix[3][1] += 1
+            np.add(H_matrix, window_matrix, out = H_matrix)
+            np.add(aa_freq_matrix, window_matrix, out = aa_freq_matrix)
+            ss_count_matrix[0][1] += 1
+            ss_count_matrix[3][1] += 1
             
         elif i == "E":
-            E_matrix += window_matrix
-            TOT_matrix += window_matrix
-            counting_matrix[1][1] += 1
-            counting_matrix[3][1] += 1
+            np.add(E_matrix, window_matrix, out = E_matrix)
+            np.add(aa_freq_matrix, window_matrix, out = aa_freq_matrix)
+            ss_count_matrix[1][1] += 1
+            ss_count_matrix[3][1] += 1
 
         elif i == "-":
-            C_matrix += window_matrix
-            TOT_matrix += window_matrix
-            counting_matrix[2][1] += 1
-            counting_matrix[3][1] += 1
+            np.add(C_matrix, window_matrix, out = C_matrix)
+            np.add(aa_freq_matrix, window_matrix, out = aa_freq_matrix)
+            ss_count_matrix[2][1] += 1
+            ss_count_matrix[3][1] += 1
+        
     return()
 
 
 def train_model(output_folder):
     id_list_open = open(id_list , "r")   
 
+    #obtain the path of the profile and dssp files from the list, and run the fill_matrices function on each one of them
     for file in id_list_open:
         if (file.rstrip() + ".profile")  in os.listdir(profile_folder):
             profile_file = os.path.join(profile_folder, (file.rstrip() + ".profile"))
             dssp_file = os.path.join(profile_folder, (file.rstrip() + ".dssp"))
-            fill_matrices(dssp_file, profile_file, E_matrix, H_matrix, C_matrix, TOT_matrix, counting_matrix)
-            #break
+            fill_matrices(dssp_file, profile_file, E_matrix, H_matrix, C_matrix, aa_freq_matrix, ss_count_matrix)
         else:
             continue
-    
 
-    total_number = (counting_matrix[3][1]*100)
-    normalized_H_matrix = np.divide(H_matrix, total_number)
-    
-    normalized_E_matrix = np.divide(E_matrix, total_number)
-    
-    normalized_C_matrix = np.divide(C_matrix, total_number)
-    
-    normalized_TOT_matrix = np.divide(TOT_matrix, total_number)
-    for l in range(3):
-        counting_matrix[l][2] = np.divide(counting_matrix[l][1], counting_matrix[3][1])
 
-    #*100 because the frequences must be in % and the profile contains the number not divided by 100
 
-    counting_path = os.path.join(output_folder, "COUNTING_MATRIX.txt" )
+    #normalize the matrices now filled, by dividing each element by the total number of ss 
+    total_ss_number = (ss_count_matrix[3][1])
+    normalized_H_matrix = np.divide(H_matrix, total_ss_number)
+    normalized_E_matrix = np.divide(E_matrix, total_ss_number)
+    normalized_C_matrix = np.divide(C_matrix, total_ss_number)
+    normalized_aa_freq_matrix = np.divide(aa_freq_matrix, total_ss_number)
+    for l in range(4):
+        ss_count_matrix[l][2] = np.divide(ss_count_matrix[l][1], ss_count_matrix[3][1])
+
+    #create the path for the output files
+    ss_count_path = os.path.join(output_folder, "ss_count_matrix.txt" )
     H_path = os.path.join(output_folder, "H_MATRIX.txt" )
     E_path = os.path.join(output_folder, "E_MATRIX.txt" )
     C_path = os.path.join(output_folder, "C_MATRIX.txt" )
-    TOT_PATH = os.path.join(output_folder, "TOT_MATRIX.txt")
+    aa_freq_path = os.path.join(output_folder, "aa_freq_matrix.txt")
 
-    np.savetxt(counting_path, counting_matrix, fmt='%s')
+    #save the output files in their respective paths
+    np.savetxt(ss_count_path, ss_count_matrix, fmt='%s')
     np.savetxt(H_path, normalized_H_matrix, fmt='%s')
     np.savetxt(E_path, normalized_E_matrix, fmt='%s')
     np.savetxt(C_path, normalized_C_matrix, fmt='%s')
-    np.savetxt(TOT_PATH, normalized_TOT_matrix, fmt='%s')
+    np.savetxt(aa_freq_path, normalized_aa_freq_matrix, fmt='%s')
     
     return()
+
 
 
 if __name__ == "__main__":
