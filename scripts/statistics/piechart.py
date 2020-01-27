1201c943d5ac053c27d28b61948537ea69a511d6
PelmoBolner <matteo.bolner@studio.unibo.it>
2020-01-27 17:41:54 +0100

    data visualization updates

---
+++ scripts/statistics/piechart.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sn
+import plotly.express as px
+import pandas as pd
+
+
+kingdom_dict = {"Bacteria":639, "Eukaryota":455, "Archaea":93, "Viruses":63, "Other":3}
+labels = list(kingdom_dict.keys())
+sizes = list(kingdom_dict.values())
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, labels = labels, autopct='%1.1f%%')
+plt.tight_layout()
+plt.savefig("kingdom_piechart.png")
+plt.clf()
+
+species_dict = {"H.sapiens":216, "E.coli":128, "T.thermophilus":38, "S.cerevisiae":32, "M.musculus":32, "B.subtilis":32, "T.maritima":31, "Other":736}
+labels = list(species_dict.keys())
+sizes = list(species_dict.values())
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, labels = labels, autopct='%1.0f%%')
+plt.tight_layout()
+plt.savefig("species_piechart.png")
+plt.clf()
